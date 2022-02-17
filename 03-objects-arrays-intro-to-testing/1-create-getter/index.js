be43576ce4ea85e02f12ac84920ce2368406b99b
Maksim Mirbabaev <mmirbabaev@yotateam.com>
2022-02-18 01:56:06 +0300

    [module 3] - Implement createGetter, invertObj, trimSymbols and uniq functions.

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const pathParts = path.split('.');
+  return (obj) => {
+    let result = obj;
+    for (let val of pathParts) {
+      result = result[val];
+      if (!result) {
+        return;
+      }
+    }
+    return result;
+  };
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,33 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return '';
+  }
 
+  if (!size) {
+    return string;
+  }
+
+  let result = '';
+  let prevCh = null;
+  let inARow = 1;
+
+  for (let i = 0; i < string.length; i++) {
+    if (prevCh !== string[i]) {
+      inARow = 1;
+    } else {
+      inARow++;
+    }
+
+    if (inARow <= size) {
+      result += string[i];
+    }
+
+    prevCh = string[i];
+  }
+
+  return result;
 }
+
+trimSymbols('eedaaad', 1);
